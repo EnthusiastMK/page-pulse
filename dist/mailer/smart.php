bdd46f58f73e5994528208c1a437ead133fe97f8
Martin <EnthusiastMK@outlook.com>
2020-01-17 13:28:58 +0200

    Change Size .modal__close

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'fergesrg@gmail.com';                 // Наш логин
-$mail->Password = '12312312321';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to 587
  
 $mail->setFrom('RunSmart', 'Pulse');   // От кого письмо 
-$mail->addAddress('cvsfewfweaef@fsfesf.com');     // Add a recipient
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
